3613a9bab110770828863e9d11157f272e253231
Ben-G <Benjamin.Encz@gmail.com>
2014-05-20 15:36:04 -0700

    sets up basic table view for the first screen

--- Source/MainScene.h
+++ Source/MainScene.h
@@ -8,6 +8,6 @@
 
 #import "CCNode.h"
 
-@interface MainScene : CCNode
+@interface MainScene : CCNode <CCTableViewDataSource>
 
 @end
--- Source/MainScene.m
+++ Source/MainScene.m
@@ -7,7 +7,38 @@
 //
 
 #import "MainScene.h"
+#import "CCTableView.h"
 
-@implementation MainScene
+@implementation MainScene {
+    CCNode *_tableViewContentNode;
+}
+
+- (void)didLoadFromCCB {
+    CCTableView *tableView = [[CCTableView alloc] init];
+    [_tableViewContentNode addChild:tableView];
+    tableView.contentSizeType = CCSizeTypeNormalized;
+    tableView.contentSize = CGSizeMake(1.f, 1.f);
+    tableView.dataSource = self;
+}
+
+#pragma mark - CCTableViewDataSource Protocol
+
+- (CCTableViewCell*) tableView:(CCTableView*)tableView nodeForRowAtIndex:(NSUInteger) index {
+    CCTableViewCell *cell = [[CCTableViewCell alloc] init];
+    cell.contentSizeType = CCSizeTypeMake(CCSizeUnitNormalized, CCSizeUnitPoints);
+    cell.contentSize = CGSizeMake(1.f, 50.f);
+    
+    CCNodeColor *colorNode = [CCNodeColor nodeWithColor:[CCColor colorWithRed:0.5 green:1.f blue:0.5]];
+    colorNode.contentSizeType = CCSizeTypeNormalized;
+    colorNode.contentSize = CGSizeMake(1.f, 1.f);
+    [cell addChild:colorNode];
+    
+    return cell;
+}
+- (NSUInteger) tableViewNumberOfRows:(CCTableView*) tableView {
+    return 10;
+}
+
+//- (float) tableView:(CCTableView*)tableView heightForRowAtIndex:(NSUInteger) index;
 
 @end
